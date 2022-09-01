a0fdc73dc948f300827916b48a2ba003de55b031
sato <sato@itsoft.local>
2022-09-02 08:39:44 +0900

    add controller

---
+++ EnglishMasterWebAssembly/Server/Controllers/PartOfSpeechController.cs
@@ -0,0 +1,25 @@
+﻿using EnglishMasterWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishMasterWebAssembly.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PartOfSpeechController : ControllerBase
+    {
+        private DB db;
+        public PartOfSpeechController(DB db)
+        {
+            this.db = db;
+        }
+        public async Task<IEnumerable<PartOfSpeech>> Get()
+        {
+            return await db.PartOfSpeeches.ToListAsync();
+        }
+    }
+}
---
+++ EnglishMasterWebAssembly/Server/Controllers/WordController.cs
@@ -0,0 +1,25 @@
+﻿using EnglishMasterWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishMasterWebAssembly.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+        private DB db;
+        public WordController(DB db)
+        {
+            this.db = db;
+        }
+        public async Task<IEnumerable<Word>> Get()
+        {
+            return await db.Words.ToListAsync();
+        }
+    }
+}
