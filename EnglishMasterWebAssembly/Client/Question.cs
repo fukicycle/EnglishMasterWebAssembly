da3c54dbc816d57fcac2b9c81c161cb7c6bade9a
sato <sato@itsoft.local>
2022-09-02 09:29:24 +0900

    create first page and desing

--- EnglishMasterWebAssembly/Client/Question.cs
+++ EnglishMasterWebAssembly/Client/Question.cs
@@ -1,6 +1,12 @@
-﻿namespace EnglishMasterWebAssembly.Client
+﻿using EnglishMasterWebAssembly.Shared.Models;
+
+namespace EnglishMasterWebAssembly.Client
 {
-    public class Question
+    public static class Question
     {
+        public static List<Vocabulary> GetQuestions(this List<Vocabulary> items)
+        {
+            return items.Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a).ToList();
+        }
     }
 }
