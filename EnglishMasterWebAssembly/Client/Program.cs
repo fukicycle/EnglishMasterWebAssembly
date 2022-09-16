7236ed804cfc9c5bcfe868baf390ec76b2743f06
Fuki Sato <lego.sato.4135@gmail.com>
2022-09-16 22:39:27 +0900

    過去の実績を全て保存できる機能を追加 ただし、違うブラウザからアクセスされるとリセットされる

--- EnglishMasterWebAssembly/Client/Program.cs
+++ EnglishMasterWebAssembly/Client/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.SessionStorage;
+using Blazored.LocalStorage;
 using EnglishMasterWebAssembly.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
