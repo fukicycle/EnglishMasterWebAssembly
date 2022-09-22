a94768e715a07500f9fca722f536d8dbaa12ba15
Fuki Sato <lego.sato.4135@gmail.com>
2022-09-22 22:27:55 +0900

    ４択の欄に意味が同一の物が表示されないように修正

--- EnglishMasterWebAssembly/Client/Question.cs
+++ EnglishMasterWebAssembly/Client/Question.cs
@@ -14,7 +14,9 @@ namespace EnglishMasterWebAssembly.Client
             List<Vocabulary> answers = new();
             answers.Add(vocabulary);
             answers.AddRange(items.Where(a => a.WordId != vocabulary.WordId && a.PartOfSpeechId == vocabulary.PartOfSpeechId).Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a).Take(3));
-            return answers.Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a);
+            double avg = answers.GroupBy(a => a.Meaning).Average(a => a.Count());
+            if (avg == 1) return answers.Select(a => new { a, o = Guid.NewGuid() }).OrderBy(a => a.o).Select(a => a.a);
+            return items.GetAnswer(vocabulary);
         }
     }
 }
