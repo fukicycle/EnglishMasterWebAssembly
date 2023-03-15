763e9fd9a37f6548254903dd188dd34bc85ba9a2
Fuki Sato <lego.sato.4135@gmail.com>
2023-03-15 23:01:23 +0900

    Level一覧を取得するAPIを実装

---
+++ EnglishMasterWebAssembly/Server/Controllers/LevelController.cs
@@ -0,0 +1,25 @@
+﻿using EnglishMasterWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishMasterWebAssembly.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LevelController : ControllerBase
+    {
+        private DB db;
+        public LevelController(DB db)
+        {
+            this.db = db;
+        }
+        public async Task<IEnumerable<Level>> Get()
+        {
+            return await db.Levels.ToListAsync();
+        }
+    }
+}
