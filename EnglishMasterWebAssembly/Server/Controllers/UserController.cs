fbd03957ade79ce6204d26755c4d2e334e493b9e
Fuki Sato <lego.sato.4135@gmail.com>
2023-03-17 23:38:28 +0900

    ログイン機能実装

---
+++ EnglishMasterWebAssembly/Server/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using EnglishMasterWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishMasterWebAssembly.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private DB db;
+        public UserController(DB db)
+        {
+            this.db = db;
+        }
+        public async Task<User> Get(long id)
+        {
+            var user = await db.Users.FindAsync(id);
+            if (user == null) return new User { Email = string.Empty };
+            return user;
+        }
+
+        [Route("post/login")]
+        public async Task<string> PostLogin([FromBody] User user)
+        {
+            try
+            {
+                var login = await db.Users.FirstOrDefaultAsync(a => a.Email == user.Email && a.Password == user.Password);
+                if (login == null) throw new Exception("Authentication failed.");
+                return login.Id.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
