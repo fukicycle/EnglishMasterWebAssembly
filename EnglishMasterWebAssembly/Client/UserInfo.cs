35e900b613a2623c9ebc6aa64910d737c5330d54
Fuki Sato <106070646+fukicycle@users.noreply.github.com>
2023-05-15 14:34:26 +0900

    実施結果を保存できるように修正 API実行時のモデル検証を無効にした

---
+++ EnglishMasterWebAssembly/Client/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using Blazored.LocalStorage;
+using EnglishMasterWebAssembly.Shared.Models;
+using System.Net.Http.Json;
+
+namespace EnglishMasterWebAssembly.Client
+{
+    public sealed class UserInfo
+    {
+        public static async Task<User?> GetUserInfo(ILocalStorageService localStorage, HttpClient http)
+        {
+            var userId = await localStorage.GetItemAsync<long>("USER_ID");
+            return await http.GetFromJsonAsync<User>($"User?id={userId}");
+        }
+    }
+}
---
+++ EnglishMasterWebAssembly/Server/Controllers/MeaningOfWordLerningHistoryController.cs
@@ -0,0 +1,43 @@
+﻿using EnglishMasterWebAssembly.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System.Net;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnglishMasterWebAssembly.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MeaningOfWordLearningHistoryController : ControllerBase
+    {
+        private DB _db;
+        public MeaningOfWordLearningHistoryController(DB db)
+        {
+            this._db = db;
+        }
+        [HttpGet("all")]
+        public async Task<IEnumerable<MeaningOfWordLearningHistory>> Get()
+        {
+            return await _db.MeaningOfWordLearningHistories.ToListAsync();
+        }
+
+
+        public async Task<IActionResult> Post([FromBody] MeaningOfWordLearningHistory meaningOfWordLearningHistory)
+        {
+            try
+            {
+                _db.MeaningOfWordLearningHistories.Add(meaningOfWordLearningHistory);
+                await _db.SaveChangesAsync();
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
